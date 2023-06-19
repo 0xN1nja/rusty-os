84e2344f880e20a790184c94ba0d6904dea4a818
Abhimanyu Sharma <speedcuberabhi@gmail.com>
2023-06-20 01:09:50 +0530

    Added Entry Point '_start' And Some Configuration Files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![no_std]
 #![no_main]
 
-use core::panic::{self, *};
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn on_panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
